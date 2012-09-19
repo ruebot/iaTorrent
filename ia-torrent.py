78a8432ac288902f8d23afb8359398b59156d4a9
nruest <ruestn@gmail.com>
2012-09-19 11:27:31 -0400

    json!

--- ia-torrent.py
+++ ia-torrent.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python
 
-import sys, os, re, urllib
-from BeautifulSoup import BeautifulSoup
+import sys, os, re, urllib, time, json
 
 #if len(sys.argv) != 2:
 #  print('Please verify RSS feed & download location')
 #  sys.exit(-1)
 
-feed = 'http://archive.org/services/collection-rss.php?collection=YorkUniversity&query=%28collection%3Ayorkuniversity%20AND%20format%3Apdf%29%20AND%20-mediatype%3Acollection' #sys.argv[1]      #user supplied RSS feed
+feed = 'http://archive.org/advancedsearch.php?q=%28collection%3Ayorkuniversity+AND+format%3Apdf%29+AND+-mediatype%3Acollection&fl%5B%5D=identifier&fl%5B%5D=title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=50000&page=1&output=json'
 download = '/tmp/ia-torrent'  #sys.argv[2]   #user supplied storage location
 
-data = urllib.urlopen(feed)
-soup = BeautifulSoup(data)
-items = soup.findAll('item')
+jsonData = urllib.urlopen(feed)
+data = json.load(jsonData)
+items = data["response"]["docs"]
 
 for item in items:
-  title = item.find('title').string.strip()
-  link = item.find('guid').string.strip()
-  matchObj = re.match(r'https://archive.org/details/(.*)', link, re.M|re.I)
-  identifier = matchObj.group(1)
+  identifier = item["identifier"]
+  title = item["title"]
   torrent = "https://archive.org/download/" + identifier + "/" + identifier +"_archive.torrent"
   urllib.urlretrieve(torrent, os.path.join(download, identifier + ".torrent"))
   print "Snatching: " + title + " from: " + torrent + "\n"
+  time.sleep(10)
+  print torrent
+
+jsonData.close()
