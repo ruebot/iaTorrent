f0844bfd468e535dec753a01e4d5b31bafc7aaf9
nruest <ruestn@gmail.com>
2013-01-10 10:00:26 -0500

    :sweat_drops: -- get rid of pandoc stuff here. not necessary for dist

--- setup.py
+++ setup.py
@@ -1,11 +1,5 @@
 from setuptools import setup
 
-import pandoc                  
-pandoc.core.PANDOC_PATH = '/usr/bin/pandoc'
-
-doc = pandoc.Document()        
-doc.markdown = open('README.md').read()
-
 description = \
     """
     This module can be used to snatch torrents for each collection object from a given Internet archive collection.
@@ -15,13 +9,11 @@ setup(
     name = 'iaTorrent',
     version = '0.1.4',
     url = 'https://github.com/ruebot/ia-torrent',
-    install_requires=['pyandoc'],
     author = 'Nick Ruest',
     author_email = 'ruestn@gmail.com',
     py_modules = ['iaTorrent'],
     scripts = ['iaTorrent.py'],
     description = description,
-    long_description = doc.rst,
     platforms = ['POSIX'],
     test_suite = 'test',
     classifiers = [
